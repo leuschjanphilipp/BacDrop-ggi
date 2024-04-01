db6958f4865d5232028d9150641f7dc2f196dabb
Jan-Philipp Leusch <leusch.jan.p@gmail.com>
2024-04-01 19:25:23 +0200

    update of analysis

--- utils/utils.py
+++ utils/utils.py
@@ -2,4 +2,4 @@ import numpy as np
 
 def calc_sparsity(matrix):
     bool_matrix = np.array(matrix, dtype=bool)
-    return matrix.sum() / np.array(matrix.shape).prod()
+    return 1 - (matrix.sum() / np.array(matrix.shape).prod())
