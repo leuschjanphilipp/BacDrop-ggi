fc94a4d3e673c862f1eefaa58c24245ab53f2b01
Jan-Philipp Leusch <leusch.jan.p@gmail.com>
2024-04-20 11:20:08 +0200

    preprocessing revamp

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,12 @@
 import numpy as np
+
 from Bio import Entrez
 from tqdm import tqdm
+from scipy.stats import median_abs_deviation
 
 def calc_sparsity(matrix):
     bool_matrix = np.array(matrix, dtype=bool)
-    return 1 - (matrix.sum() / np.array(matrix.shape).prod())
+    return 1 - (bool_matrix.sum() / np.array(bool_matrix.shape).prod())
 
 
 def fetch_protein_names(accession_numbers):
@@ -16,3 +18,10 @@ def fetch_protein_names(accession_numbers):
         if record.startswith(">"):
             names.append(record.strip().lstrip(">"))
     return names
+
+def is_outlier(adata, metric: str, nmads: int):
+    M = adata.obs[metric]
+    outlier = (M < np.median(M) - nmads * median_abs_deviation(M)) | (
+        np.median(M) + nmads * median_abs_deviation(M) < M
+    )
+    return outlier
